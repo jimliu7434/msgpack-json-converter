eca9cd689a27eebb9a3f3b1286145ee965dfecbf
jimliu7434 <jimliu7434@gmail.com>
2023-10-29 22:52:15 +0800

    修正 int parser 問題

--- pkg/msgpack/code/number.go
+++ pkg/msgpack/code/number.go
@@ -7,33 +7,31 @@ import (
 )
 
 const (
-	_PrefixPosFixInt = 0b00000000
-	_PrefixNegFixInt = 0b11100000
-	_PrefixInt8      = 0xd0
-	_PrefixInt16     = 0xd1
-	_PrefixInt32     = 0xd2
-	_PrefixInt64     = 0xd3
-	_PrefixFloat32   = 0xca
-	_PrefixFloat64   = 0xcb
+	_PrefixInt8    = 0xd0
+	_PrefixInt16   = 0xd1
+	_PrefixInt32   = 0xd2
+	_PrefixInt64   = 0xd3
+	_PrefixFloat32 = 0xca
+	_PrefixFloat64 = 0xcb
 )
 
 var (
-	//_IntShortRange = []int64{-16, 63}
-	_Int8Range    = []int64{-128, 127}                                 // 2^7 = 128
-	_Int16Range   = []int64{-32768, 32767}                             // 2^15 = 32768
-	_Int32Range   = []int64{-2147483648, 2147483647}                   // 2^31 = 2147483648
-	_Int64Range   = []int64{-9223372036854775808, 9223372036854775807} // 2^63 = 9223372036854775808
-	_Float32Range = []float64{-3.40282346638528859811704183484516925440e+38, 3.40282346638528859811704183484516925440e+38}
-	_Float64Range = []float64{-1.797693134862315708145274237317043567981e+308, 1.797693134862315708145274237317043567981e+308}
+	_IntShortRange = []int64{-16, 63}                                   // positive 7bit, negative 5bit
+	_Int8Range     = []int64{-128, 127}                                 // 2^7 = 128
+	_Int16Range    = []int64{-32768, 32767}                             // 2^15 = 32768
+	_Int32Range    = []int64{-2147483648, 2147483647}                   // 2^31 = 2147483648
+	_Int64Range    = []int64{-9223372036854775808, 9223372036854775807} // 2^63 = 9223372036854775808
+	_Float32Range  = []float64{-3.40282346638528859811704183484516925440e+38, 3.40282346638528859811704183484516925440e+38}
+	_Float64Range  = []float64{-1.797693134862315708145274237317043567981e+308, 1.797693134862315708145274237317043567981e+308}
 )
 
 func IntFunc(v int64) ([]byte, error) {
 	switch {
-	// case v >= _IntShortRange[0] && v <= _IntShortRange[1]:
-	// 	if v < 0 {
-	// 		return []byte{_PrefixNegFixInt + byte(v)}, nil
-	// 	}
-	// 	return []byte{_PrefixPosFixInt + byte(v)}, nil
+	case v >= _IntShortRange[0] && v <= _IntShortRange[1]:
+		if v < 0 {
+			return []byte{byte(v)}, nil
+		}
+		return []byte{byte(v)}, nil
 	case v >= _Int8Range[0] && v <= _Int8Range[1]:
 		return []byte{_PrefixInt8, byte(v)}, nil
 	case v >= _Int16Range[0] && v <= _Int16Range[1]:
